3acf1a83e18812410482c8877f3f65f1db264ace
deleav <a42006310@gmail.com>
2017-11-24 23:14:38 +0800

    Fix bug If there are multiple 'src' in the full path of 'kuromoji', it would replace from first 'src'.

--- dist/node/kuroshiro.js
+++ dist/node/kuroshiro.js
@@ -369,7 +369,7 @@ var init = function(options, callback){
 
     var dicPath = options.dicPath;
     if(!dicPath){
-        if(isNode) dicPath = require.resolve('kuromoji').replace(/src.*/,'dict/');
+        if(isNode) dicPath = require.resolve('kuromoji').replace(/src(?!.*src).*/,'dict/');
         else dicPath = 'bower_components/kuroshiro/dist/dict/';
     }
     kuromoji.builder({ dicPath: dicPath }).build(function (err, newtokenizer) {
